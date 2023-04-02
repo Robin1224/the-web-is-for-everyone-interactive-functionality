2fd307481e679405565b9d656a2b652b65a1c255
Robin1224 <81151231+Robin1224@users.noreply.github.com>
2023-04-03 00:44:04 +0200

    a

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Import dependencies
 import express from "express"; // import the express module
 import dotenv from "dotenv"; // import the dotenv module
-import { index } from "./helpers/routes.js"; // import the router object from the routes.js file
+import { axiosGet, axiosPost } from "./helpers/axiosWrapper.js";
 
 dotenv.config(); // load environment variables from a .env file
 
@@ -9,7 +9,16 @@ const app = express(); // creating an instance of the express application
 
 app.set("view engine", "ejs"); // setting the view engine to EJS
 
-app.get("/", index); // setting the index route to the index function in the routes.js file;
+app.get("/", (request, response) => {
+  const url = process.env.API_URL;
+
+  const data = axiosGet(`${process.env.API_URL}/methods`, process.env.LOG_ERRORS).then((data) => {
+    console.log(data);
+    if (data != "error") {
+      response.render('index', data);
+    }
+  })
+});
 
 app.listen(3000, () => { // starting the server and listening on port 3000
   console.log("Server is running on port 3000");
